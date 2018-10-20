63e8c1077ac5f50a74d8fb21d89044740c6d0209
Jonathan Reckinger <jpreckinger@gmail.com>
2018-10-20 14:01:56 -0500

    debugged intial issues, properly sourced scripts, adjusted 'start' in package.json, commented out route/database code for later use

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -1,44 +1,45 @@
-const express = require('express');
-const taskRouter = express.Router();
+//const express = require('express');
+//const taskRouter = express.Router();
 
 //Connection to database
-const pg = require('pg');
-const url = require('url');
+// const pg = require('pg');
+// const url = require('url');
 
-const Pool = pg.Pool;
+// const Pool = pg.Pool;
 
-let config = {};
+// let config = {};
 
-if (process.env.DATABASE_URL ) {
-    const params = url.parse(process.env.DATABASE_URL);
-    const auth = params.auth.split(':');
+// if (process.env.DATABASE_URL ) {
+//     const params = url.parse(process.env.DATABASE_URL);
+//     const auth = params.auth.split(':');
 
-    config = {
-        user: auth[0],
-        password: auth[1],
-        host: params.hostname,
-        port: params.port,
-        database: params.pathname.split('/')[1],
-        ssl: true,
-        max: 10,
-        idleTimeoutMillis: 30000
-    }
-}
-else {
-    config = {
-        database: 'weekend-to-do-app',
-        host: 'localhost',
-        port: 5432,
-        max: 10,
-        idleTimeoutMillis: 30000
-    }
-}
-const pool = new Pool(config);
+//     config = {
+//         user: auth[0],
+//         password: auth[1],
+//         host: params.hostname,
+//         port: params.port,
+//         database: params.pathname.split('/')[1],
+//         ssl: true,
+//         max: 10,
+//         idleTimeoutMillis: 30000
+//     }
+// }
+// else {
+//     config = {
+//         database: 'weekend-to-do-app',
+//         host: 'localhost',
+//         port: 5432,
+//         max: 10,
+//         idleTimeoutMillis: 30000
+//     }
+// }
+// const pool = new Pool(config);
 
-pool.on('connect', () => {
-   console.log('Connected to the database');
-});
+// pool.on('connect', () => {
+//    console.log('Connected to the database');
+// });
 
-pool.on('error', () => {
-   console.log('Error with database pool');
-});
+// pool.on('error', () => {
+//    console.log('Error with database pool');
+// });
+
