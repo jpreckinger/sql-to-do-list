399c25b750f328f29a0b740ac9138cec5dd5314d
Jonathan Reckinger <jpreckinger@gmail.com>
2018-10-20 13:17:10 -0500

    initial commit, set up file structure and boilerplate for server.js and index.html

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const router;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
