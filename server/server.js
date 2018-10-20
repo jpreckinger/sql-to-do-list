04e9a04699c1d420611b8cdb872ba62f0796f6f3
Jonathan Reckinger <jpreckinger@gmail.com>
2018-10-20 13:44:25 -0500

    added database connectivity and a router file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
+// boilerplate setup for server file
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-const router;
+const taskRouter = require('./routes/task.router');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
+//routes
+app.use('/tasks', taskRouter);
+
+
+//Spinning up port
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 })
